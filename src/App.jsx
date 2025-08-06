89b8cfe7a18d5926886f66eaf8712fbbe0e5bed0
manulzvz13 <manulzvz@gmail.com>
2025-08-06 16:13:21 -0600

    feat: Add AboutPage Component and Header component with Route and Link via react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
-import HomePage from "./pages/home";
 import { Routes, Route } from "react-router";
+import Header from "./components/Header";
+import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -31,24 +33,28 @@ const App = () => {
   }, [limit]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <HomePage
-            coins={coins}
-            filter={filter}
-            setFilter={setFilter}
-            limit={limit}
-            setLimit={setLimit}
-            sortBy={sortBy}
-            setSortBy={setSortBy}
-            loading={loading}
-            error={error}
-          />
-        }
-      ></Route>
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              filter={filter}
+              setFilter={setFilter}
+              limit={limit}
+              setLimit={setLimit}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              loading={loading}
+              error={error}
+            />
+          }
+        />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 };
 
