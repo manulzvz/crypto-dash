8ea2a17b0d9e5ff3a5fccd3d1e14dc6a498a523b
manulzvz13 <manulzvz@gmail.com>
2025-08-06 09:53:13 -0600

    feat: Add initial API testing to console

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useState, useEffect } from "react";
+const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cup_desc&per_page=10&page=1&sparkline=false';
+
 const App = () => {
+  const [coints, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => {
+        if(!res.ok) throw new Error("Failed to fetch data");
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCoints(data);
+        setLoading(false);
+      })
+      .catch((err)=> {
+        setError(err.message);
+        setLoading(false);
+      })
+  }, []);
+
   return ( 
     <div>
       <h1>🚀 Crypto Dash</h1>
