2df748b703a4e2c5b04f01cc3f27c58c4bb8ba5c
manulzvz13 <manulzvz@gmail.com>
2025-08-06 16:30:06 -0600

    feat: Add NotFoundPage component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import Header from "./components/Header";
 import HomePage from "./pages/home";
 import AboutPage from "./pages/about";
+import NotFoundPage from "./pages/not-found";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -53,6 +54,7 @@ const App = () => {
           }
         />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
